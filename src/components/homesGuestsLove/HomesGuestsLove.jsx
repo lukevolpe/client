d8a01b07d2a37e52abcae04bff9f48cddd4183fa
Luke Volpe <luke.volpe@gmail.com>
2023-12-30 21:11:27 +0000

    Finished integrating homepage

--- src/components/homesGuestsLove/HomesGuestsLove.jsx
+++ src/components/homesGuestsLove/HomesGuestsLove.jsx
@@ -1,60 +1,35 @@
+import useFetch from '../../hooks/useFetch';
 import './homesGuestsLove.css';
 
 const HomesGuestsLove = () => {
+  const { data, loading, error } = useFetch(
+    '/api/hotels?featured=true&limit=4'
+  );
+
   return (
     <div className='homesGuestsLove'>
-      <div className='glItem'>
-        <img
-          src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o='
-          className='glImg'
-        />
-        <span className='glName'>Aparthotel Stare Miasto</span>
-        <span className='glCity'>Old Town, Poland, Kraków</span>
-        <span className='glPrice'>Starting from £106</span>
-        <div className='glRating'>
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-      <div className='glItem'>
-        <img
-          src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o='
-          className='glImg'
-        />
-        <span className='glName'>Aparthotel Stare Miasto</span>
-        <span className='glCity'>Old Town, Poland, Kraków</span>
-        <span className='glPrice'>Starting from £106</span>
-        <div className='glRating'>
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-      <div className='glItem'>
-        <img
-          src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o='
-          className='glImg'
-        />
-        <span className='glName'>Aparthotel Stare Miasto</span>
-        <span className='glCity'>Old Town, Poland, Kraków</span>
-        <span className='glPrice'>Starting from £106</span>
-        <div className='glRating'>
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
-      <div className='glItem'>
-        <img
-          src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o='
-          className='glImg'
-        />
-        <span className='glName'>Aparthotel Stare Miasto</span>
-        <span className='glCity'>Old Town, Poland, Kraków</span>
-        <span className='glPrice'>Starting from £106</span>
-        <div className='glRating'>
-          <button>8.9</button>
-          <span>Excellent</span>
-        </div>
-      </div>
+      {loading ? (
+        'Loading...'
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className='glItem' key={item._id}>
+              <img src={item.photos[0]} className='glImg' />
+              <span className='glName'>{item.name}</span>
+              <span className='glCity'>{item.city}</span>
+              <span className='glPrice'>
+                Starting from £{item.cheapestPrice}
+              </span>
+              {item.rating && (
+                <div className='glRating'>
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 };
